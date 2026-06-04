1b4c2455774345bc4d874fe18687bcbc65c4ba28
Slev <H138082@outlook.com>
2026-06-04 13:48:00 +0800

    定义SequenceItem类

---
+++ README.md
@@ -0,0 +1,5 @@
+# 序列与迭代器
+
+## Task1
+源代码：[SequenceItem](src/main/java/com/example/sequence/SequenceItem.java)
+
---
+++ src/main/java/com/example/sequence/SequenceItem.java
@@ -0,0 +1,20 @@
+package com.example.sequence;
+
+public class SequenceItem {
+    private String data;
+
+    public SequenceItem() {
+    }
+
+    public SequenceItem(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
